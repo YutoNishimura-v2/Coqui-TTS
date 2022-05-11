bdbe119992ea8cf262e69fa6c2d859c19547341a
yuto_nishimura <yutonishimurav20512@gmail.com>
2022-05-11 20:01:10 +0900

    pro+ave+morikawa start

--- run_at_abci.sh
+++ run_at_abci.sh
@@ -1,5 +1,5 @@
 # 例: qsub_Ag1 -l h_rt='10:00:00' -o ~/logs/yourTTS/20220429_pro_eng_4_202205012016.log run_at_abci.sh
-# 例: qsub_Afull -l h_rt='70:00:00' -o ~/logs/yourTTS/20220429_pro_eng_3_202205081841.log run_at_abci.sh
+# 例: qsub_Afull -l h_rt='70:00:00' -o ~/logs/yourTTS/20220511_pro_averuni_202205111950.log run_at_abci.sh
 # 例: qrsh -g $ABCI_GROUP -l rt_AG.small=1 -l h_rt=10:00:00
 # 例: qrsh -g $ABCI_GROUP -l rt_AF=1 -l h_rt=10:00:00
 
@@ -20,7 +20,7 @@ export WANDB_API_KEY=372c44d0dd36d935650a41082a67b4ae2cb80015
 
 # multi-GPU
 python3 TTS/bin/distribute.py --script TTS/bin/train_tts.py \
-    --config_path exps/20220429_pro_eng_3/config.json \
-    --restore_path checkpoints/20220429_pro_eng_3/20220429_pro_eng_3-April-30-2022_01+29AM-7f3c80ab/checkpoint_50000.pth.tar
+    --config_path exps/20220511_pro_averuni/config.json \
+    --restore_path exps/20220425_pro_eng_2/pretrained_model_from_opensource_del_text_emb.pth
 
 deactivate
